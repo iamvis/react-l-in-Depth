0eaaebd1daa8f2be7d1ccd04779178ca6e118fbb
iamvis <prjapativishal82141@gmail.com>
2026-02-23 22:13:32 +0530

    E-5-updated

--- E-5/src/components/Body.js
+++ E-5/src/components/Body.js
@@ -1,11 +1,28 @@
+import { useEffect, useState } from "react"
 import { restList } from "../utils/demoData"
 import RestoCart from "./RestoCart"
 
 const Body=()=>{
+   const [restoretList, setRestorentList]= useState(restList);
+   useEffect(()=>{
+    console.log("useEffect is called");
+   },[]);
+
     return <>
             <div className="rs-c" >
         {/* <RestoCart/> */}
-      { restList.map((restorent)=>(
+        <div className="filter">
+          <button className="filter-rating-btn"
+          onClick={(e)=>{
+                const filterList = restList.filter((restorent)=>restorent.info.avgRating>4)
+                setRestorentList(filterList);
+              }}
+          >top rated rastorent</button>
+          <button className="filter-location-btn">
+            location
+          </button>
+        </div>
+      { restoretList.map((restorent)=>(
         <RestoCart key={restorent.info.id} restData={restorent}/>
       ))
         
